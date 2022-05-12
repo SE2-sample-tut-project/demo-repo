caa04660f32f97fb23aa52261481cd830b29ee2d
Fabian <oraze.fabian@icloud.com>
2022-05-12 10:36:23 +0200

    commit all files

---
+++ app/src/main/java/com/example/demo_repo/SampleClass.java
@@ -0,0 +1,7 @@
+package com.example.demo_repo;
+
+public class SampleClass {
+    public static int addition(int i, int j) {
+        return i + j;
+    }
+}
---
+++ app/src/test/java/com/example/demo_repo/ExampleUnitTest.java
@@ -0,0 +1,18 @@
+package com.example.demo_repo;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, SampleClass.addition(2,2));
+    }
+}
